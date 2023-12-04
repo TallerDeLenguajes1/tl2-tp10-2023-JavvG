70aac377c1074263698006561dd48b62a64cc93c
Javier Granero <javiergranero89@gmail.com>
2023-12-04 15:20:14 -0300

    Correcciones menores

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -16,7 +16,7 @@ public class Usuario
 
     public int Id { get => id; set => id = value; }
     public string Nombre { get => nombre; set => nombre = value; }
-    public string Password { get => password; set => password = value};
-    public Rol Rol { get => rol; set => rol = value};
+    public string Password { get => password; set => password = value;}
+    public Rol Rol { get => rol; set => rol = value;}
 
 }
