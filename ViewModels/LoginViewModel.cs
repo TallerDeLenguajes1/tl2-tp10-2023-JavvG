da550511c6531de4755406a3f4268cab5f381947
Javier Granero <javiergranero89@gmail.com>
2023-12-04 17:08:42 -0300

    Se crean las carpetas para almacenar los ViewModels

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace tl2_tp10_2023_JavvG.ViewModels
+{
+
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Este campo es requerido")]        // Atributo de validación
+        [Display(Name = "Nombre de usuario")]
+        public string NombreUsuario {get; set;}   
+        
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [PasswordPropertyText]
+        [Display(Name = "Contraseña")]
+        public string Password {get; set;}
+
+        public LoginViewModel() {}
+
+    }
+
+}
