aea4a4b3e82160cc1b41ce85aa89152ad54bb213
Javier Granero <javiergranero89@gmail.com>
2023-12-04 12:44:04 -0300

    Se añadió un nuevo enum con los roles posibles del usuario (Administrador u Operador). Se añaden campos a la tabla Usuario de la BD.

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -3,10 +3,20 @@ namespace tl2_tp10_2023_JavvG.Models;
 public class Usuario 
 {
 
+    public enum Rol 
+    {
+        administrador,
+        operador
+    }
+
     private int id;
     private string nombre;
+    private string password;
+    private Rol rol;
 
     public int Id { get => id; set => id = value; }
     public string Nombre { get => nombre; set => nombre = value; }
+    public string Password { get => password; set => password = value};
+    public Rol Rol { get => rol; set => rol = value};
 
 }
