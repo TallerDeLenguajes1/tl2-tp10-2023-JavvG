0c47b562d3fb38c019d1d50fc891084fd27695c5
Javier Granero <javiergranero89@gmail.com>
2023-12-09 13:48:03 -0300

    Se agrega un ViewModel para la creación de tareas

---
+++ ViewModels/CrearTareaViewModel.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using tl2_tp10_2023_JavvG.Models;
+
+namespace tl2_tp10_2023_JavvG.ViewModels;
+
+public class CrearTareaViewModel 
+{
+    [Display(Name = "ID")]
+    public int Id { get; set; }
+
+    [Display(Name = "ID Tablero")]
+    public int IdTablero { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [Display(Name = "Título")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [Display(Name = "Descripción")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [Display(Name = "Color")]
+    public string Color { get; set; }
+
+    [Display(Name = "Estado")]
+    public EstadoTarea Estado { get; set; }
+
+    [Display(Name = "ID del usuario asignado")]
+    public int IdUsuarioAsignado { get; set; }
+}
