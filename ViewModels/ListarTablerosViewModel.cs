1ab1c0e1152cbc282341909e084940b3f81a040b
Javier Granero <javiergranero89@gmail.com>
2024-01-25 01:43:16 -0300

    Se añaden dos nuevas propiedades: listas de objetos Tablero que almacenan los tableros propios y aquellos en los que el usuario logueado tenga tareas asignadas. Se modifican y agregan contructores conforme a las modificaciones mencionadas.

--- ViewModels/ListarTablerosViewModel.cs
+++ ViewModels/ListarTablerosViewModel.cs
@@ -6,16 +6,31 @@ namespace tl2_tp10_2023_JavvG.ViewModels;
 
 public class ListarTablerosViewModel 
 {
-    [Display(Name = "Tableros")]
-    public List<Tablero> Tableros { get; set; }
-
     public int IdUsuario { get; set; }
 
-    public ListarTablerosViewModel() {}
+    public List<Tablero> Tableros { get; set; }
+
+    public List<Tablero> TablerosPropios { get; set; }
+
+    public List<Tablero> TablerosConTareasAsignadas { get; set; }
+
+    public ListarTablerosViewModel() 
+    {
+        Tableros = new();
+        TablerosPropios = new();
+        TablerosConTareasAsignadas = new();
+    }
 
     public ListarTablerosViewModel(List<Tablero> tableros, int idUsuario)
     {
         Tableros = tableros;
+        IdUsuario = idUsuario;
+    }
+
+    public ListarTablerosViewModel(List<Tablero> tableros1, List<Tablero> tableros2, int idUsuario)
+    {
+        TablerosPropios = tableros1;
+        TablerosConTareasAsignadas = tableros2;
         IdUsuario = idUsuario;
     }
 }
